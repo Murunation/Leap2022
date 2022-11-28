dde493f1e9f40acce82212fba0fed0a5abbf9f80
Morning <morning@admins-iMac.local>
2022-11-28 11:01:07 +0800

    task1

---
+++ js/basic/function.js
@@ -0,0 +1,39 @@
+// let name = "Hello World";
+// let myNumber1 = 15;
+// let myNumber2 = 30;
+// let myNumber3 = myNumber1 + myNumber2;
+// let isTrue = true;
+
+// let numb1 = 100,
+//   numb3 = 200;
+
+// myNumber1 = 100;
+// myNumber1 = "changed";
+// console.log(name);
+// console.log(myNumber1);
+// console.log(numb3);
+
+// let name = "Murun";
+// var surname = "MO";
+// const PI = 3.14;
+// PI = 3.16;
+// console.log(PI);
+
+// number string ylgaa
+// let num1 = "16";
+// let num2 = "17";
+
+// console.log(num1 + num2); String uchraas ene bolohgu harin number uyd possible
+// let num1 = "16";
+// let num2 = "17";
+// let num3 = 18;
+// let num4 = 19;
+// console.log(num3 + num4);
+let num1 = 15;
+let num2 = 10;
+console.log("This is addition of 2 given numbers", num1 + num2);
+
+console.log("Subtraction of given two numbers is", num1 - num2);
+console.log("This is a multiply of 2 given numbers", num1 * num2);
+console.log("This is a divide of 2 given numbers", num1 / num2);
+console.log("This is uldeldel of 2 given numbers", num1 % num2);
