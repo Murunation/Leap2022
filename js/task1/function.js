a0315e428bbbf23858cacfd8b112231d65eb2e20
Morning <morning@admins-iMac.local>
2022-11-28 11:00:18 +0800

    task-1

---
+++ js/task1/function.js
@@ -0,0 +1,8 @@
+let num1 = 15;
+let num2 = 10;
+console.log("This is addition of 2 given numbers", num1 + num2);
+
+console.log("Subtraction of given two numbers is", num1 - num2);
+console.log("This is a multiply of 2 given numbers", num1 * num2);
+console.log("This is a divide of 2 given numbers", num1 / num2);
+console.log("This is uldeldel of 2 given numbers", num1 % num2);
