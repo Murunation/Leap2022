1ceaa6ce1d3f9ede8e55e02f288b7926c9d5800c
Morning <morning@admins-iMac.local>
2022-11-29 12:38:47 +0800

    if condition

---
+++ js/if/function.js
@@ -0,0 +1,144 @@
+// let day = 3;
+// let udur;
+// if (day === 1) {
+//   udur = "Monday";
+// } else if (day === 2) {
+//   udur = "Tuesday";
+// } else if (day === 3) {
+//   udur = "Wednesday";
+// } else if (day === 4) {
+//   udur = "Thursday";
+// } else if (day === 5) {
+//   udur = "Friday";
+// } else if (day === 6) {
+//   udur = "Saturday";
+// } else if (day === 7) {
+//   udur = "Sunday";
+// } else {
+//   udur = "Unknown";
+// }
+// console.log("Today is " + udur);
+
+// let myAge = 23;
+// if (myAge < 11) {
+//   console.log("Baby");
+// } else if (myAge < 24) {
+//   console.log("Teeenager");
+// } else if (myAge < 40) {
+//   console.log("Adult");
+// } else console.log("Old");
+
+// if (weekday =>1 && weekday <=7 ){
+//     if (weekday==1){
+//         weekname=
+//     }
+// }
+
+// if (day === 1) {
+//   udur = "Monday";
+// } else if (day === 2) {
+//   udur = "Tuesday";
+// } else if (day === 3) {
+//   udur = "Wednesday";
+// } else if (day === 4) {
+//   udur = "Thursday";
+// }
+
+// let day = 4;
+// switch (day) {
+//   case 1:
+//     console.log("Monday");
+//     break;
+//   case 2:
+//     console.log("Tuesday");
+//     break;
+//   case 3:
+//     console.log("Wednesday");
+//     break;
+//   case 4:
+//     console.log("Thursday");
+//     break;
+//   case 5:
+//     console.log("Friday");
+//     break;
+//   case 6:
+//     console.log("Saturday");
+//     break;
+//   case 7:
+//     console.log("Sunday");
+//     break;
+//   default:
+//     console.log("Not a day");
+// }
+// let weekday = 4;
+// if (
+//   weekday == 1 ||
+//   weekday == 2 ||
+//   weekday == 3 ||
+//   weekday == 4 ||
+//   weekday == 5
+// ) {
+//   console.log("Working day");
+// }
+
+// let x =15;
+// if ( x>10 && x<20){
+//     console.log("10 to 20");
+// } else ( x<=10 || x>=20);
+
+// let x = 20,
+//   y = 35;
+// z = 30;
+
+// if (x > y) {
+//   if (x > z) {
+//     console.log(x, "hamgiin in");
+//   } else {
+//     console.log(z, "hamgiin ih");
+//   }
+// } else {
+//   if (y > z) {
+//     console.log(y, "hamgiin ih");
+//   } else {
+//     console.log(z, "hamgiin ih");
+//   }
+// }
+
+// if (x > y && x > z) {
+//   console.log(x, "hamgiin ih");
+// } else if (y > z) {
+//   console.log(y, "hamgiin ih");
+// } else {
+//   console.log(z, "hamgiin ih");
+// }
+
+// let number = 17;
+// if (number % 2 == 1) {
+//   console.log(number, "Even Number");
+// }
+
+// buhel toog awdag
+// let num1 = 7;
+// console.log(parseInt(num1 / 3));
+
+// Oppp Ternary ;
+// let age = 15;
+// if (age < 21) {
+//   console.log("You aare not allowed to drink alchohol");
+// } else {
+//   console.log("You aare allowed to drink alchohol");
+// }
+
+// age < 21
+//   ? console.log("You aare not allowed to drink alchohol")
+//   : console.log("You aare allowed to drink alchohol");
+
+// condition ? true : false;
+
+// let myState = age >= 10 && age <= 18 ? "teenager" : "adult";
+// const myState = age >= 10 && age <= 18 ? "teenager" : "adult";
+
+let year = 2800;
+if ((year % 400 == 0 || year % 100 != 0) && year % 4 == 0)
+  console.log("leap year mun");
+else console.log("Leap year bish");
