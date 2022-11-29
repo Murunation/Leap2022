1f0db0b8c994f700dfbd3651e04e6fe231632f08
Murunation <murunbattulga6@gmail.com>
2022-11-30 00:40:54 +0800

    hamgiin ih

---
+++ js/day2-task/higher.js
@@ -0,0 +1,17 @@
+let x = 10, y =11, z=9;
+if (x > y) {
+  if (x > z) {
+    console.log(x, "hamgiin in");
+  } else  {
+    console.log(z, "hamgiin ih");
+    console.log(y, "hamgiin baga");
+  }
+} else {
+  if (y > z) {
+    console.log(y, "hamgiin ih");
+    // console.log(z, "hamgiin baga");
+  } else {
+    console.log(z, "hamgiin ih");
+    console.log(x, "hamgiin baga");
+  }
+}
