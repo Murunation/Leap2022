8698b1d9e0e1ac9a2a7f3a37623eb9098c0dffc1
Murunation <murunbattulga6@gmail.com>
2022-11-29 23:55:43 +0800

    huwisagch

---
+++ js/day2-task/function.js
@@ -0,0 +1,22 @@
+let a = 3, b = 7, c=2, d=4;
+let N = 1;
+let urjwerHad = 1;
+if (a>=5) {
+} else   {
+    urjwerHad = N*a;
+}if (b>=5) {
+}
+else {
+    urjwerHad = urjwerHad*b; 
+} if (c>=5) {
+}
+else  {
+    urjwerHad = urjwerHad*c;
+    
+ }if (d>=5) {
+ }
+ else {
+    urjwerHad = urjwerHad *d; 
+ }
+console.log(urjwerHad);
+
---
+++ js/day2-task/sum.js
@@ -0,0 +1,17 @@
+let a = 85, b=75, c=96, d=69;
+let sum =0;
+let test = 0;
+if (a>=80) {
+    sum = a;
+} else {
+} if (b>=80) {
+    sum = sum + a;
+} else {
+} if (c>=80) {
+    sum = sum + c;
+} else {
+} if (d>=80) {
+    sum = sum + d;
+} else{
+}
+console.log(sum);
