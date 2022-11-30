1abc640581dc472c9e2b01889ea2192c845e7bc7
Morning <morning@admins-iMac.local>
2022-11-30 12:20:33 +0800

    simple assignment

--- js/day3-task/functions.js
+++ js/day3-task/functions.js
@@ -1,20 +1,27 @@
-let oneTime = 'Hello I am "Murun" \nI am "21" years old';
-console.log(oneTime);
-console.log(oneTime.toLowerCase());
-console.log(oneTime.toUpperCase());
+// let oneTime = 'Hello I am "Murun" \nI am "21" years old';
+// console.log(oneTime);
+// console.log(oneTime.toLowerCase());
+// console.log(oneTime.toUpperCase());
 
-let welcome = "Welcome to ";
-let pine = "Pinecone academy";
-let S = welcome.concat(pine);
-console.log(S);
+// let welcome = "Welcome to ";
+// let pine = "Pinecone academy";
+// let S = welcome.concat(pine);
+// console.log(S);
 
-let firstName = "Murun";
-let lastName = "Battulga.";
-let country = "Mongolia.";
-let age = 21;
-let sentence =
-  firstName.concat(" " + lastName) + " " + "I'm".concat(" " + country);
-console.log(sentence);
+// let firstName = "Murun";
+// let lastName = "Battulga.";
+// let country = "Mongolia.";
+// let age = 21;
+// let sentence =
+//   firstName.concat(" " + lastName) + " " + "I'm".concat(" " + country);
+// console.log(sentence);
 
-let ex6 = "1 2 3 4 5 \n2 3 4 5 1 \n3 4 5 1 2 \n4 5 1 2 3 \n5 1 2 3 4";
-console.log(ex6);
+// let ex6 = "1 2 3 4 5 \n2 3 4 5 1 \n3 4 5 1 2 \n4 5 1 2 3 \n5 1 2 3 4";
+// console.log(ex6);
+
+console.log(Math.floor(Math.random() * 100) + 1);
+console.log(Math.floor(Math.random() * 100) + 1);
+console.log(Math.floor(Math.random() * 100) + 1);
+
+console.log(Math.floor(Math.random() * 255) + 1);
+console.log(Math.floor(Math.random() * 255) + 1);
