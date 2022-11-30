5d3c41f8808ce5c0ce8d9a5cd0d49285bbbc9d28
Morning <morning@admins-iMac.local>
2022-11-30 09:17:45 +0800

    assignment

---
+++ js/day2-task/assignment.js
@@ -0,0 +1,49 @@
+// let i = 4 > 3; // true
+// console.log(i);
+// let iii = 4 < 3; //false
+// console.log(iii);
+// let ii = 4 >= 3; //true
+// console.log(ii);
+// let iv = 4 <= 3; //false
+// console.log(iv);
+// let v = 4 == 4; //true
+// console.log(v);
+// let vi = 4 === 4; //true
+// console.log(vi);
+// let vii = 4 != 4; //false
+// console.log(vii);
+// let iix = 4 !== 4; //false
+// console.log(iix);
+// let ix = 4 != "4"; //false
+// console.log(ix);
+// let x = 4 == "4"; //true
+// console.log(x);
+// let xi = 4 === "4"; //false
+// console.log(xi);
+
+// let one = 4 > 3 && 10 > 12; //false
+// console.log(one);
+// let two = 4 > 3 && 10 < 12; //true
+// console.log(two);
+// let three = 4 > 3 || 10 < 12; //true
+// console.log(three);
+// let four = 4 > 3 || 10 > 12; //true
+// console.log(four);
+// let five = !(4 > 3); //false
+// console.log(five);
+// let six = !(4 < 3); //true
+// console.log(six);
+// let seven = !(4 > 3 && 10 < 12); //false
+// console.log(seven);
+// let eight = !(4 > 3 && 10 > 12); //true
+// console.log(eight);
+// let nine = !(4 === "4"); //true
+// console.log(nine);
+
+// let A = 5 + " myAge";
+// let B = 4 + "yourAge";
+// if (A > B) {
+//   console.log("Bi chamaas ah um bna haha");
+// } else {
+//   console.log("Bi chamaas duu um bna haha");
+// }
--- js/day2-task/leap.js
+++ js/day2-task/leap.js
@@ -1,4 +1,4 @@
-let year = 2400;
-if ((year % 400 == 0 || year % 100 != 0) && year % 4 == 0)
-  console.log("leap year mun");
-else console.log("Leap year bish");
+// let year = 2400;
+// if ((year % 400 == 0 || year % 100 != 0) && year % 4 == 0)
+//   console.log("leap year mun");
+// else console.log("Leap year bish");
