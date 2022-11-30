2dba7c0bba0f69867871ac62f4c0caf8a1a823b8
Morning <morning@admins-iMac.local>
2022-11-30 12:06:13 +0800

    simple

--- js/day3-task/functions.js
+++ js/day3-task/functions.js
@@ -1,2 +1,20 @@
 let oneTime = 'Hello I am "Murun" \nI am "21" years old';
 console.log(oneTime);
+console.log(oneTime.toLowerCase());
+console.log(oneTime.toUpperCase());
+
+let welcome = "Welcome to ";
+let pine = "Pinecone academy";
+let S = welcome.concat(pine);
+console.log(S);
+
+let firstName = "Murun";
+let lastName = "Battulga.";
+let country = "Mongolia.";
+let age = 21;
+let sentence =
+  firstName.concat(" " + lastName) + " " + "I'm".concat(" " + country);
+console.log(sentence);
+
+let ex6 = "1 2 3 4 5 \n2 3 4 5 1 \n3 4 5 1 2 \n4 5 1 2 3 \n5 1 2 3 4";
+console.log(ex6);
