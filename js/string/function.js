a12479ebd75cd822ba1f08f1ce94e3d7c81bb673
Morning <morning@admins-iMac.local>
2022-11-30 12:26:11 +0800

    string

---
+++ js/string/function.js
@@ -0,0 +1,100 @@
+// // (\) this is white space(Special case uudiig gargaj irdeg)
+// // (')
+// // (\n) new line, like a space.
+// // (\\) hoyulang n hewlejgargaj irehiiin tuld
+// // (\t) goy eh tsegtstei haragduulmaar bwel
+// // (\") textn dotor quitetio mark gargaj irmeer bwel ingej tawi
+// // let myString = "single quotes' double quotes\" ";
+// // let babyShark =
+// //   "Baby Shark Baby Shark \nBaby Shark Baby Shark \tBaby Shark Baby Shark";
+// // // let niit = myString.concat(babyShark); //niiluulehed
+// // let fullString = myString.concat(" ", "This is concat");
+// // console.log(fullString);
+// // console.log(myString);
+// // //.substr Tuhain zuilees tedees tediin hoorond utgiig awna.
+// // console.log(myString.substring(0, 6));
+// // console.log(myString.substring(7, 13));
+
+// // //length niit too space terend orno
+// // console.log(babyShark.length);
+// // console.log(babyShark.charAt(5));
+// // console.log(babyShark.toUpperCase()); //Bugdiin tomruulah
+// // console.log(babyShark[babyShark.length - 1]); //<== hamgiin suulin temdegtiig awahad.
+
+// // property
+// // .length
+
+// //METHOD
+// // Ardaas zaawal haalt awna method iin araas
+// // tuhain method 0  ees ehelne
+// // tend bairlah temdegt
+// //Tuhain zuilees neg negeer umiig salgaj awahad ashiglana.
+// // .charAt() = [1] <== Barag ylgaagu(shuud stringiin ardaas bichne tseg ashiglahgui)
+
+// // //Bairshil oilgoh .indexof();
+// // let str = "This is a string";
+// // console.log(str.indexOf("is"));
+// // let firstPart = str.substring(0, str.indexOf("is"));
+// // let lastPart = str.substring(str.indexOf("is") + "is".length, str.length);
+// // console.log(lastPart);
+// // str = firstPart.concat(lastPart);
+
+// // firstPart = str.substring(0, str.indexOf("is"));
+// // lastPart = str.substring(str.indexOf("is") + "is".length, str.length);
+// // str = firstPart.concat(lastPart);
+// // console.log(str);
+
+// //NUMBER
+
+// // console.log(Number.isInteger(5.01));
+
+// //.toFixed()
+// // let number = 123.5698;
+// // let dNumber = number.toFixed(2); //123.54
+// // console.log(dNumber); //123.54
+// // console.log(number);
+
+// // // too bichwer to easy
+// // const budger = 10_000_000;
+// // console.log(budger);
+
+// // Math PI
+// // 3.1445566
+
+// // MATH abs
+// // Eyreg bnu surug bnu bugdiin eyreg bolgonooo
+// // let a=5, b=9;
+// // console.log(Math.abs;
+
+// //Math.ceil (Ar taliin butarhaig ceil buyu taaszruu deesheegee tataad hewlene.)
+// // console.log(Math.ceil(0.95));
+// // // output 1
+
+// // console.log(Math.ceil(7.0004));
+// // //output 8
+
+// //MATH floor (Ar taliin butarhaig floor buyu shalruu dooshoogoo tataad hewlene.)
+// console.log(Math.floor(2.95));
+// // output 1
+
+// console.log(Math.floor(7.0004));
+// //output 7
+
+// // Math.round deeshee dooshoo bish yg defaultaar
+// console.log(Math.round(7.0004));
+// //output 7
+
+// // Math.max hamgiin ihiin awna. math.min bagin away
+// console.log(Math.max(1, 3, 4, 2));
+// //output: 4
+// console.log(Math.max(-1, -3, -4, -2));
+// //output: -1
+
+// //Math.pow
+// console.log(Math.pow(7, 3));
+// //output 343
+
+// //Math.random()
+// console.log(Math.floor(Math.random(1) * 6) + 1); //Floor gesnii uchir n random too uguhud urjeed doosh n tataad buhel bolgono. uchir n butarhai too randommoor ucgh mgdgu.
+
+console.log(Math.floor(Math.random() * (Math.abs(a - b) + 1)) + Math.min(a.b));
