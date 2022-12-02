e1f338987d027828802e32ad974726d198f017fa
Morning <morning@admins-iMac.local>
2022-12-02 12:34:19 +0800

    for break

---
+++ js/for,break/function.js
@@ -0,0 +1,109 @@
+// let text = "";
+// for (let i = 0; i < 10; i++) {
+//   if (i === 3) {
+//     break;
+//   }
+//   text += "The number is " + i + ". ";
+// }
+// console.log(text);
+
+// let hour = 21;
+// if (hour < 10) {
+//   console.log("Good Morning");
+// } else if (hour < 20) {
+//   console.log("Good Day");
+// } else {
+//   console.log("Good evening");
+// }
+
+//FOR
+// let i = 1;
+// while (i <= 10) {
+//   console.log(i);
+//   i++;
+// }
+
+// ****
+// *  *
+// *  *
+// ****
+// let output = "";
+// let n = 7;
+
+// for (let row = 1; row <= n; row++) {
+//   //This for is row
+//   //things to i while condition is true
+//   for (let col = 1; col <= n; col++) {
+//     if (col <= row && col == row) {
+//       output += " " + "*";
+//     } else {
+//       output += "  ";
+//     }
+//     //this for is col
+//   }
+//   output += "\n";
+//   debugger;
+// }
+// console.log(output);
+// let =1 uildel
+// condition 2 uildel
+// uildel buyu console.log = 3 uildel
+// daraa n i++ ajilna
+// (Dahij Let ajillahgui, condition 1 duger uildlees ehleed ywna)
+// Durem
+// for (let; condition; update) {}
+
+//Suulees n 3 dahi ornii "_" hiih
+// let number = 1234567897899;
+// console.log(number.charAt(number.length - 3));
+// console.log(number.substring(1, 3));
+// number =
+//   number.substring(0, number.length - 3) +
+//   "_" +
+//   number.substring(number.length - 3, number.length);
+// console.log(number);
+
+//Password
+// let password = "1234";
+// let input;
+// while (password !== input) {
+//   input = prompt("Enter your password");
+// }
+
+// phone number validation
+// let number;
+
+// while (true) {
+//   number = prompt("Enter a phone number");
+//   let firstChar = number.charAt(0);
+//   number = Number(number);
+//   if (Number.isInteger(number) && (firstChar == "9" || firstChar == "8")) {
+//     break;
+//   }
+// }
+
+// Continue
+// Continue dooshoo uildel hiihgu algasnaa recycle again
+// let i = 0;
+// while (i < 10) {
+//   i++;
+//   if (i % 2 == 0) {
+//     continue;
+//   }
+//   console.log(i);
+// }
+
+// anhnii too
+// let n = 20;
+// let prime = true;
+// for (let i = 2; i < n; i++) {
+//   if (n % i == 0) {
+//     prime = false;
+//     break;
+//   }
+// }
+// if (prime) {
+//   console.log("YES");
+// } else {
+//   console.log("NO");
+// }
