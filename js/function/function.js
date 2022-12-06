bf6ec3497931cdd7ae1a88f348ebe02682fd6be0
Morning <morning@admins-iMac.local>
2022-12-06 12:35:12 +0800

    function

---
+++ js/function/function.js
@@ -0,0 +1,101 @@
+//Function vvsgeh helber
+// function myFunction(parameter1, parameter2) {
+//   //code of function
+// }
+// //function duudah helber
+// myFunction(1, 2, 3);
+
+//Example 1 //Hariu butsaadaggui function
+//Hello there
+function greet() {
+  console.log("Hello there");
+}
+
+//Call the function
+greet();
+greet();
+
+//result
+//Hello there
+
+// Example 2
+function withName(name) {
+  console.log("Hello" + name);
+}
+withName(" and goodbye");
+
+// Example 3 : 2 utga awaad vrjwer
+function myFunction(p1, p2) {
+  return p1 * p2;
+}
+
+//Function n zuwhun hereglesen uyd l result aa butsaadag
+myFunction(2, 3);
+// result
+// Nothing why?
+// Def: Zarlaj uguhgui baigaa.
+
+//Sum of three
+function sumofThree(num1, num2, num3) {
+  return num1 + num2 + num3;
+}
+let result = sumofThree(1, 2, 3); //not step 1
+// console.log(sumofThree(1, 2, 3)); // step 2 but not
+console.log(result); //update of ster 2
+// result Sum of three number : 6
+
+//ex Sum of Three prompt alert
+// let number1 = parseFloat(prompt("Enter first number: "));
+// let number2 = parseFloat(prompt("Enter second number: "));
+// function sumofTwo(number1, number2) {
+//   return number1 + number2;
+// }
+// let result2 = sumofTwo(number1, number2);
+// alert("This sum is" + " " + result2);
+// another ex
+// let number1 = parseFloat(prompt("Enter first number: "));
+// let number2 = parseFloat(prompt("Enter second number: "));
+// let result = add (number1,number2)
+// console.log(result);
+// function(number1,number2){
+//     return number1 + number2;
+// }
+
+// Ex: highest number of threee
+// let A = parseFloat(prompt("Enter first number: "));
+// let B = parseFloat(prompt("Enter second number: "));
+// let C = parseFloat(prompt("Enter Third number: "));
+// let max = 0;
+// function maxNum(A, B, C) {
+//   if (A < B) {
+//     if (B > C) {
+//       max = B;
+//     } else {
+//       max = C;
+//     }
+//   } else {
+//     if (A > C);
+//     {
+//       max = A;
+//     }
+//     max = C;
+//   }
+//   return max;
+// }
+// let resultii = maxNum(A, B, C);
+// alert(resultii);
+
+// let a = function (name) {
+//   return "hello" + name; // Same
+// };
+// let b = (name) => {
+//   return "Hello" + name;
+// }; // This is a arrow function/ const is better than let
+// console.log(b + "wonderland");
+
+// let upper = prompt("Enter: ");
+// function changingUpper(upper) {
+//   return changingUpper;
+// }
+// let txt = upper.toUpperCase();
+// alert(txt);
---
+++ js/function/sorter.js
@@ -0,0 +1,5 @@
+const arr = [4, 32, 2, 5, 6, 10, -10, 1, 0, -20];
+arr.sort(function arrey(a, b) {
+  return a - b;
+});
+console.log(arr);
