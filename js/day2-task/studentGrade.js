1417df8e86d64b92232673a0a3cb71fd4bb076c0
Murunation <murunbattulga6@gmail.com>
2022-12-11 19:52:37 +0800

    student

---
+++ js/day2-task/studentGrade.js
@@ -0,0 +1,41 @@
+let Bat = 95;
+let Dumd = 59;
+let review = "";
+if (0 < Bat && Bat <= 59){
+    review = "Маш муу";
+    console.log("Name: Bat - Grade: " + Bat + " - review: " + review);
+} else  if (60 < Bat && Bat <= 69) {
+    review = "Хангалттай";
+    console.log("Name: Bat - Grade: " + Bat + " - review: " + review);
+}
+ else if (70 < Bat && Bat <= 79) {
+    review = "Дунд";
+    console.log("Name: Bat - Grade: " + Bat + " - review: " + review);
+}
+ else if (80 < Bat && Bat <= 89) {
+    review = "Сайн";
+    console.log("Name: Bat - Grade: " + Bat + " - review: " + review);
+} else if (90 < Bat && Bat <= 100) {
+    review = "Маш Сайн";
+    console.log("Name: Bat - Grade: " + Bat + " - review: " + review );
+}
+
+//Dumd
+if (0 < Dumd && Dumd <= 59){
+    review = "Маш муу";
+    console.log("Name: Dumd - Grade: " + Dumd + " - review: " + review);
+} else  if (60 < Dumd && Dumd <= 69) {
+    review = "Хангалттай";
+    console.log("Name: Dumd - Grade: " + Dumd + " - review: " + review);
+}
+ else if (70 < Dumd && Dumd <= 79) {
+    review = "Дунд";
+    console.log("Name: Dumd - Grade: " + Dumd + " - review: " + review);
+}
+ else if (80 < Dumd && Dumd <= 89) {
+    review = "Сайн";
+    console.log("Name: Dumd - Grade: " + Dumd + " - review: " + review);
+} else if (90 < Dumd && Dumd <= 100) {
+    review = "Маш Сайн";
+    console.log("Name: Dumd - Grade: " + Dumd + " - review: " + review );
+}
