fe79f545f97454248dabe12745ae2365b7f0f82c
Morning <morning@admins-iMac.local>
2022-12-14 12:33:21 +0800

    thats color

--- js/DOM/changeColor/color.js
+++ js/DOM/changeColor/color.js
@@ -1,13 +1,10 @@
 let button = document.querySelector("#myBtn");
+let para = document.querySelectorAll("h2");
 button.addEventListener("click", function () {
-  document.getElementById(
-    "htwoofTwo"
-  ).style.backgroundColor = `rgb(${Math.floor(
+  para[0].style.backgroundColor = `rgb(${Math.floor(
     Math.random() * 255
   )}, ${Math.floor(Math.random() * 255)},${Math.floor(Math.random() * 255)})`;
-  document.getElementById(
-    "htwoofOne"
-  ).style.backgroundColor = `rgb(${Math.floor(
+  para[1].style.backgroundColor = `rgb(${Math.floor(
     Math.random() * 255
   )}, ${Math.floor(Math.random() * 255)},${Math.floor(Math.random() * 255)})`;
 });
